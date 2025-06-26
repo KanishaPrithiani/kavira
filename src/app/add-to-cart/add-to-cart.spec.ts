c7321b29f667aa490b6530f3a52173b96e3a3c38
kavyavala485 <kavyavala485@gmail.com>
2025-06-26 17:58:34 +0530

    changes

--- src/app/add-to-cart/add-to-cart.spec.ts
+++ src/app/add-to-cart/add-to-cart.spec.ts
@@ -1,23 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddToCart } from './add-to-cart';
+// import { AddToCart } from './add-to-cart';
 
-describe('AddToCart', () => {
-  let component: AddToCart;
-  let fixture: ComponentFixture<AddToCart>;
+// describe('AddToCart', () => {
+//   let component: AddToCart;
+//   let fixture: ComponentFixture<AddToCart>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AddToCart]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [AddToCart]
+//     })
+//     .compileComponents();
 
-    fixture = TestBed.createComponent(AddToCart);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(AddToCart);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
+
+
+
+
+
+
+// import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-add-to-cart',
+//   templateUrl: './add-to-cart.component.html',
+//   styleUrls: ['./add-to-cart.component.scss']
+// })
+// export class AddToCartComponent {
+//   constructor(private router: Router) {}
+
+//   goHome() {
+//     this.router.navigate(['/home']); // 👈 Change '/home' if your homepage route is different
+//   }
+// }
