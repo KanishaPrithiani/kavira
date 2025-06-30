15251e96e79d7ef988b851e1de557c9aa9afaab8
kavyavala485 <kavyavala485@gmail.com>
2025-06-30 13:18:50 +0530

    msg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,18 @@
 // import { Routes } from '@angular/router';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUs } from './about-us/about-us'; 
-import { Gifts } from './gifts/gifts';
+// import { Gifts } from './gifts/gifts';
 import { AddToCart } from './add-to-cart/add-to-cart';
 import { ContactUs } from './contact-us/contact-us';
 import { Home } from './home/home';
+import { Component, HostListener } from '@angular/core';
+
 
 
  export const routes: Routes = [
     {path:'', component: Home},
     {path:'aboutus', component: AboutUs},
-    {path:'gifts', component: Gifts},
+    // {path:'gifts', component: Gifts},
     {path:'addtocart', component: AddToCart},
     {path:'contactus', component: ContactUs},
 ]
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgSelectModule],
   standalone: true,  
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'Kavira';
+
+  constructor(
+    private router: Router
+  ) {}
+
+    isDropdownOpen = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown() {
+    this.isDropdownOpen = false;
+  }
+  
+  giftOptions = [
+    { label: 'Gift Combos', route: '/gift-combos' },
+    { label: 'Build Your Own', route: '/build-your-own' },
+    { label: 'Chocolates', route: '/chocolates' },
+    { label: 'Flowers', route: '/flowers' }
+  ];
+  onGiftSelect(selected: any) {
+  if (selected && selected.route) {
+    this.router.navigate([selected.route]);
+  }
+
 }
 
 
-
+}
 
