5bdc589cc5d7cda83e69f269a4d90a4cbed3e4bc
Kanisha <kanishaprithiani13@gmail.com>
2025-06-30 13:23:07 +0530

    chances

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 // import { Routes } from '@angular/router';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUs } from './about-us/about-us'; 
-import { Gifts } from './gifts/gifts';
+import { GiftsComponent} from './gifts/gifts';
 import { AddToCart } from './add-to-cart/add-to-cart';
 import { ContactUs } from './contact-us/contact-us';
 import { Home } from './home/home';
@@ -10,7 +10,7 @@ import { Home } from './home/home';
  export const routes: Routes = [
     {path:'', component: Home},
     {path:'aboutus', component: AboutUs},
-    {path:'gifts', component: Gifts},
+    {path:'gifts', component: GiftsComponent},
     {path:'addtocart', component: AddToCart},
     {path:'contactus', component: ContactUs},
 ]
--- src/app/gifts/gifts.ts
+++ src/app/gifts/gifts.ts
@@ -1,11 +1,54 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-gifts',
+//   imports: [],
+//   templateUrl: './gifts.html',
+//   styleUrl: './gifts.scss'
+// })
+// export class Gifts {
+
+// }
+
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gifts',
-  imports: [],
   templateUrl: './gifts.html',
-  styleUrl: './gifts.scss'
+  styleUrls: ['./gifts.scss']
 })
-export class Gifts {
+export class GiftsComponent {
+  selectedProduct: any = null;
 
+  products = [
+    {
+      id: 1,
+      title: 'Creamy Berries Chocolate Bar',
+      price: 295,
+      image: 'assets/img1.png',
+      description: 'Indulge in the rich and creamy taste of Baileys x SMOOR...',
+      ingredients: ['Sugar', 'Cocoa Butter', 'Milk Solids'],
+      weight: '100g'
+    },
+    {
+      id: 2,
+      title: 'Luxury Truffle Box',
+      price: 1500,
+      image: 'assets/img2.PNG',
+      description: 'Elegant truffles made with Belgian chocolate and hazelnut.',
+      ingredients: ['Chocolate', 'Hazelnut', 'Cream'],
+      weight: '200g'
+    }
+    // Add more products as needed
+  ];
+
+  selectProduct(product: any) {
+    console.log("Image Clicked:", product.title);
+    this.selectedProduct = product;
+  }
+
+  goBack() {
+    this.selectedProduct = null;
+  }
 }
